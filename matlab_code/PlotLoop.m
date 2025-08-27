31c7e617274419fbfd1c56949350f735b2f65a15
Zhengbing He <50189549+gotrafficgo@users.noreply.github.com>
2025-08-27 13:48:17 -0400

    Delete matlab_code directory

--- matlab_code/PlotLoop.m
+++
@@ -1,9 +0,0 @@
-function PlotLoop(Config, LoopLocationList)
-    
-    % Plot each horizontal line
-    for i = 1:length(LoopLocationList)
-        y = LoopLocationList(i);
-        plot([Config.TimeStart, Config.TimeEnd], [y, y], 'k-', 'LineWidth', 1); hold on;
-    end    
-    
-end
